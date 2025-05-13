5a00d47bdf3e720f5e9d288f6684dd08aca4de15
oscell <82882938+oscell@users.noreply.github.com>
2025-05-13 23:25:18 +0200

    cleaned up cards

--- src/components/Hit.tsx
+++ src/components/Hit.tsx
@@ -1,8 +1,6 @@
-import * as React from "react";
 import { useHits } from "react-instantsearch";
 
 import { Highlight } from "react-instantsearch";
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -11,25 +9,11 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 
-type HitType = {
-  objectID: string;
-  name: string;
-  description: string;
-  framework: string;
-};
+import type { Hit } from "algoliasearch";
 
 type HitProps = {
-  hit: HitType;
+  hit: Hit;
 };
 
 function Hit({ hit }: HitProps) {
@@ -43,38 +27,8 @@ function Hit({ hit }: HitProps) {
           <Highlight attribute="description" hit={hit} />
         </CardDescription>
       </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor={`name-${hit.objectID}`}>Name</Label>
-              <Input
-                id={`name-${hit.objectID}`}
-                placeholder="Name of your project"
-                defaultValue={hit.name}
-              />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor={`framework-${hit.objectID}`}>Framework</Label>
-              <Select defaultValue={hit.framework}>
-                <SelectTrigger id={`framework-${hit.objectID}`}>
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                  <SelectItem value="next">Next.js</SelectItem>
-                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                  <SelectItem value="astro">Astro</SelectItem>
-                  <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button variant="outline">Cancel</Button>
-        <Button>Deploy</Button>
-      </CardFooter>
+      <CardContent></CardContent>
+      <CardFooter className="flex justify-between"></CardFooter>
     </Card>
   );
 }
@@ -85,9 +39,7 @@ export function CustomHits(props: any) {
   return (
     <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       {items.map((hit) => (
-        <div
-          key={hit.objectID}
-        >
+        <div key={hit.objectID}>
           <Hit hit={hit} />
         </div>
       ))}
